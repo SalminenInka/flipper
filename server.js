1d835adbdc0fad860d791c7225e2b4cc45c8a49b
inka <inka_salminen@hotmail.com>
2022-05-03 20:35:41 +0300

    Flipper newshit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const got = require('got');
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router()
+
+
+
+
+app.listen(3000);
